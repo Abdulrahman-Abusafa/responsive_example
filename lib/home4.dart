90ce3734a958c29e354fe0cd1cd93cad87884564
Abdulrahman <abdualrahman.abusafa@gmail.com>
2021-08-15 14:10:06 +0300

    some changes

--- lib/home4.dart
+++ lib/home4.dart
@@ -75,16 +75,33 @@ class ImageIN extends StatelessWidget {
         ),
         Padding(
           padding: const EdgeInsets.all(50.0),
-          child: Text(
-            ' this is a big mountain That you can not climb',
-            style: TextStyle(color: Colors.white, fontSize: fontSize),
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Text(
+                ' this is a big mountain That you can not climb',
+                style: TextStyle(color: Colors.white, fontSize: fontSize),
+              ),
+              if (Responsive.isDesktop(context))
+                Column(
+                  children: [
+                    SizedBox(height: 150),
+                    Align(
+                      child: ElevatedButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.amber[600]),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('enter now'),
+                          )),
+                      alignment: Alignment.bottomCenter,
+                    )
+                  ],
+                )
+            ],
           ),
         ),
-        if (Responsive.isDesktop(context))
-          Align(
-            child: ElevatedButton(onPressed: () {}, child: Text('enter now')),
-            alignment: Alignment.bottomCenter,
-          )
       ],
     );
   }
